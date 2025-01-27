dd9d41e6d3c6fcd40066dc303e17f35cadb41c19
karmakaragradwip02 <karmakaragradwip02@gmail.com>
2025-01-27 15:42:25 +0530

    streamlit

---
+++ DEPLOY_MODEL_STREAMLIT/2-app.py
@@ -0,0 +1,32 @@
+import os
+import time
+import streamlit as st
+import boto3
+
+s3 = boto3.client('s3')
+
+bucket_name = "tinybertaws"
+
+local_path = 'DEPLOY_MODEL_STREAMLIT/downloads/tinybert-sentiment-analysis'
+s3_pefix = 'mlmodel-aws/tinybert-sentiment-analysis/'
+
+def download_folder(local_path, s3_prefix, bucket_name):
+    os.makedirs(local_path, exist_ok=True)
+
+    paginator = s3.get_paginator('list_objects_v2')
+    for result in paginator.paginate(Bucket=bucket_name, Prefix=s3_prefix):
+        if 'Contents' in result:
+            for Key in result['Contents']:
+                s3_key = Key['Key']
+
+                local_file = os.path.join(local_path, os.path.relpath(s3_key, s3_prefix))
+                os.makedirs(os.path.dirname(local_file), exist_ok=True)
+
+                s3.download_file(bucket_name, s3_key, local_file)
+
+st.title("Machine Learning Model Deployment At The Server !!!")
+button = st.button("Download Model")
+if button:
+    with st.spinner("Downloading........... Please Wait"):
+        download_folder(local_path, s3_pefix, bucket_name)
+        st.text("Model Downloading finished 😊😊😊")
